61dc233d5af37cbcb9892f7fcf61e4da44b05d89
Alessandra Albertoni <alessandra.albertoni@gmail.com>
2021-10-26 15:00:11 -0300

    Teste de Git Course2

---
+++ Course_2/Produto.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Course_2 {
+    class Produto {
+        private string _nome;
+        //auto properties
+        public double Preco { get; private set; }
+        public double Quantidade { get; private set; }
+      
+
+        public Produto() {
+        }
+
+      
+        public Produto(string nome, double preco, int quantidade) {
+            _nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        // properties
+        public string Nome {
+            get { return _nome; }
+            set {
+                if (value != null && value.Length > 1) {
+                    _nome = value;
+                }
+            }
+        }
+
+        /*public double Preco {
+            get { return _preco; }
+        }
+
+        public int Quantidade {
+            get {
+                return _quantidade;}
+        }
+
+                public string GetNome() {
+            return _nome;
+        }*/
+
+        /*
+        metodo manual
+        public void SetNome(string nome) {
+            if (nome != null && nome.Length > 1) {
+                _nome = nome;
+            }
+        }
+        public double GetPreco() {
+            return _preco;
+        }
+        public int GetQuantidade() {
+            return _quantidade;
+        }*/
+
+        public double ValorTotalEmEstoque() {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade) {
+            Quantidade = Quantidade + quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade) {
+            Quantidade = Quantidade - quantidade;
+        }
+
+
+        public override string ToString() {
+            return _nome
+                  + ", $"
+                  + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                  + ", "
+                  + Quantidade
+                  + " unidades, Total: $ "
+                  + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Course_2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course_2 {
+    class Program {
+        static void Main(string[] args) {
+
+            //Produto p = new Produto("TV",500.00,10);
+            //Console.WriteLine(p);
+
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //Console.Write("Quantidade no estoque: ");
+            //int quantidade = int.Parse(Console.ReadLine());
+
+            Produto p = new Produto(nome, preco);
+
+            Produto p2 = new Produto();
+
+            Produto p3 = new Produto() { Nome = "TV", Preco = 500.00, Quantidade = 20 };
+           
+            Console.WriteLine("Dados do Produto: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado do estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido ao estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: " + p);
+
+        }
+    }
+}
